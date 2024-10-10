49c3f6012140a9498452f5eb12bc55c1eb8e9c31
Johan Rodriguez <you@example.com>
2024-10-10 12:24:03 -0500

    feat: add common utilities and helpers
    added utility files in src/common/utils: database_collection.ts, database_enum.ts, joi-error.ts and added helpers in src/common/utils/helpers: joiDefaultErrorMessagesHelper.ts, httpErrorsHelper.ts

---
+++ src/common/utils/helpers/joiDefaultErrorMessagesHelper.ts
@@ -0,0 +1,14 @@
+export default {
+  "any.required": "El campo «{{#label}}» es requerido",
+  "string.base": "El campo «{{#label}}» no es una cadena de texto válida",
+  "string.empty": "El campo «{{#label}}» no puede estar vacío",
+  "string.min":
+    "El campo «{{#label}}» debe tener al menos {{#limit}} caracteres",
+  "string.max":
+    "El campo «{{#label}}» debe tener menos de {{#limit}} caracteres",
+  "string.pattern.base":
+    "El campo «{{#label}}» no cumple con el formato «{{#regex}}»",
+  "number.base": "El campo «{{#label}}» no es un número válido",
+  "number.min": "El campo «{{#label}}» debe ser mayor o igual a {{#limit}}",
+  "number.max": "El campo «{{#label}}» debe ser menor o igual a {{#limit}}",
+};
